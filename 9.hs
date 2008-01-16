ade7d111a32f40efc1dad36a6030a8fc7f4b249b
Justin Bogner <im@justinbogner.com>
2008-01-15 21:54:12 -0700

    nicer 9

--- 9.hs
+++ 9.hs
@@ -1,2 +1,1 @@
-main = putStrLn . show . $ [ (a,b,c) | a <- [1..999], b <- [1..a], let c = isqrt (a^2+b^2), a+b+c==1000, c^2==a^2+b^2 ]
-isqrt = floor . sqrt . fromIntegral
+main = putStrLn . show . head $ [ a*b*c | a <- [1..999], b <- [1..a], let c = 1000-a-b, c^2==a^2+b^2 ]
