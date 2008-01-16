2abafc54654c369d73fb427b2f8e69f2458d1c12
Justin Bogner <im@justinbogner.com>
2008-01-15 19:51:35 -0700

    9 still not done

--- 9.hs
+++ 9.hs
@@ -1,2 +1,6 @@
-main = putStrLn . show $ [ c | a <- squares, b <- squares, c2 <- zipWith (+) a b, elem c squares, a+b+c==1000]
-squares = map (^2) . [0..1000]
+main = putStrLn . show $ [ (a,b,c) | a <- squares, b <- squares, c2 <- (+) a b, isSquare c2, c <- isqrt c2, a+b+c==1000]
+squares = map (^2) [0..1000]
+isSquare :: Int -> Bool
+isSquare = flip elem squares
+--sumSquares x y = filter (flip elem squares) $ zipWith (+) x y
+isqrt = floor . sqrt . fromIntegral
