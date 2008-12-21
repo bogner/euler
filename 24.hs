d502db77d27bce665feba72b2b7a19356c80359e
Justin Bogner <mail@justinbogner.com>
2008-12-21 02:13:55 -0700

    lengths of base 10 integers

--- 24.hs
+++ 24.hs
@@ -1,7 +1,5 @@
 main = putStrLn . show $ p24 1000
 
-p24 n = head $ dropWhile ((<n) . digits . snd) (zip [1..] fibs)
-
-digits = length . show
+p24 n = head $ dropWhile ((<10^999) . snd) (zip [1..] fibs)
 
 fibs = 1:1:zipWith (+) fibs (tail fibs)
